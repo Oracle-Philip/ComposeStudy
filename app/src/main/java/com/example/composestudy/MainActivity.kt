d9c56bdce7be89066da3b5ef83b3a41177a7261a
PhilipY <philipy075@gmail.com>
2023-08-14 15:04:11 +0900

    compose에서 livedata 사용방법
    - observeAsState사용

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,16 +38,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
-
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.material3.Scaffold
-
-
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -57,10 +52,13 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.launch
@@ -73,33 +71,65 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //compose안에서 viewmodel 사용하기
-            val viewModel = viewModel<MainViewModel>()
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ){
-                Text(
-                    viewModel.data.value,
-                    fontSize = 30.sp
-                )
-                Button(onClick = {
-                    //viewModel.data.value = "World"
-                    viewModel.changeValue()
-                }){
-                    Text("변경")
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
-class MainViewModel : ViewModel() {
-    private val _data = mutableStateOf("Hello")
-    val data : State<String> = _data
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(viewModel: MainViewModel = viewModel()){
+    val text1 : MutableState<String> = remember {
+        mutableStateOf("Hello World")
+    }
 
-    fun changeValue(){
-        _data.value = "world"
+    var text2 : String by remember {
+        mutableStateOf("Hello World")
+    }
+
+    val (text : String, setText: (String) -> Unit) = remember {
+        mutableStateOf("Hello World")
+    }
+
+    //observeAsState를 통해 Compose에서 liveData 사용 할 수 있다.
+    //kotlin flow도 Compose에서 제공해준다.
+    val text3 = viewModel.liveData.observeAsState("Hello World")
+
+    Column() {
+        Text("Hello World")
+        Button(onClick = {
+            text1.value = "변경"
+            println(text1.value)
+            text2 = "변경"
+            println(text2)
+            setText("변경")
+            viewModel.changeValue("변경")
+//            println(text)
+        }){
+            Text("클릭")
+        }
+        //recomposition ==> 다시 그려지는 행위..
+        TextField(value = text, onValueChange = setText)
+    }
+}
+
+class MainViewModel: ViewModel(){
+    /**
+     * @Stable
+    interface MutableState<T> : State<T> {
+    override var value: T
+    operator fun component1(): T  ==> getter
+    operator fun component2(): (T) -> Unit  ==> setter
+    }
+     */
+    private val _value : MutableState<String> = mutableStateOf("Hello World")
+    //State:읽기만 가능
+    val value : State<String> = _value
+
+    private val _liveData = MutableLiveData<String>()
+    val liveData: LiveData<String> = _liveData
+
+    fun changeValue(value: String){
+        _value.value = value
     }
 }

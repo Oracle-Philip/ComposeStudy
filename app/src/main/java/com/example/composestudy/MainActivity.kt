50db8b6f705e69ed1fd853e21ef7ea5e9be3ad7c
PhilipY <philipy075@gmail.com>
2023-08-15 10:24:11 +0900

    수평계 앱
    - canvas와 센서를 활용한

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.example.composestudy
 
 import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Canvas
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -62,11 +71,17 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -76,116 +91,101 @@ import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter",
-        "UnusedMaterialScaffoldPaddingParameter"
-    )
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //화면 꺼지지 않도록 하기
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        //화면 가로 모드로 고정되게 하기
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
+
+        //Activity에는 lifecycle 객체가 존재한다.
+        lifecycle.addObserver(viewModel)
+
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "home"){
-                composable(route = "home"){
-                    HomeScreen(navController)
-                }
-                composable(route = "resulut"){
-                    ResultScreen(navController, bmi = 12.0)
-                }
-            }
+            TiltScreen(
+                x = viewModel.x.value,
+                y = viewModel.y.value
+            )
         }
     }
 }
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("ServiceCast")
+class MainViewModel(application: Application) : AndroidViewModel(application), SensorEventListener,
+    LifecycleObserver {
+
+    private val _x = mutableStateOf(0f)
+    val x : State<Float> = _x
+
+    private val _y = mutableStateOf(0f)
+    val y : State<Float> = _y
+
+    private val sensorManager by lazy {
+        application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun registerSensor(){
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun unregisterSensor(){
+        sensorManager.unregisterListener(this)
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let{
+            _x.value = event.values[0]
+            _y.value = event.values[1]
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+}
+
 @Composable
-fun HomeScreen(navController: NavController){
-    val (height, setHeight) = rememberSaveable {
-        mutableStateOf("")
-    }
+fun TiltScreen(x: Float, y: Float){
+    
+    val yCoord = x * 20
+    val xCoord = y * 20
+    
+    Canvas(modifier = Modifier.fillMaxSize()){
+        val centerX = size.width / 2
+        val centerY = size.height / 2
 
-    val (weight, setWeight) = rememberSaveable {
-        mutableStateOf("")
-    }
-
-    Scaffold(
-        // () -> Unit 함수형태이다.
-        topBar = {
-            TopAppBar(
-                title = { Text("비만도 계산기") }
-            )
-        }
-    ) {
-        Column(
-//            modifier = Modifier.padding(PaddingValues(top = 90.dp))
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Spacer(modifier = Modifier.height(60.dp))
-
-            OutlinedTextField(
-                value = height,
-                onValueChange = setHeight,
-                label = { Text("키")},
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            )
-            OutlinedTextField(
-                value = weight,
-                onValueChange = setWeight,
-                label = { Text("몸무게")},
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Button(
-                onClick = {
-                          navController.navigate("resulut")
-                },
-                modifier = Modifier.align(Alignment.End)
-            ){
-                Text("결과")
-            }
-        }
+        drawCircle(
+            color = Color.Black,
+            radius = 100f,
+            center = Offset(centerX, centerY),
+            style = Stroke()
+        )
+        //녹색원
+        drawCircle(
+            color = Color.Green,
+            radius = 100f,
+            center = Offset(centerX + xCoord, centerY + yCoord),
+        )
+        //가운데 십자가
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX - 20, centerY),
+            end = Offset(centerX + 20, centerY)
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX, centerY - 20),
+            end = Offset(centerX, centerY + 20)
+        )
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun ResultScreen(navController: NavController, bmi: Double,){
-    Scaffold(
-        topBar = {
-            TopAppBar(title = {Text("비만도 계산기")},
-            navigationIcon = {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = "home",
-                    modifier = Modifier.clickable {
-                        navController.popBackStack()
-                    }
-                )
-            })
-        }
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            //Spacer(modifier = Modifier.height(60.dp))
-            Text("과체중", fontSize = 30.sp)
-            Spacer(modifier = Modifier.height(50.dp))
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_sentiment_dissatisfied_24),
-                contentDescription = null,
-                modifier = Modifier.size(100.dp),
-            )
-        }
-    }
-}
-
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun Preview(){
-
+    TiltScreen(x = 30f, y = 20f)
 }
